71873cba3beabb53735532e982c8ae42b7cfaa75
Miguel Fantin Dachery <miguelfantindachery@Miguels-MacBook-Air.local>
2022-06-06 00:21:17 +0100

    mapsypromises

--- src/components/ItemsListContainer/ItemsListContainer.js
+++ src/components/ItemsListContainer/ItemsListContainer.js
@@ -1,24 +1,82 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import BasicCard from "../Cards/Cards"
 import { Grid } from "@mui/material"
 
 
+
+
 const ItemsListContainer = () => {
+
+    const [products, setProducts] = useState([])
+    const productos = [
+        {
+            title: 'Remera Estilizada',
+            precio: 400,
+            stock: 12,
+            id: 1,
+        },
+        {
+            title: 'Remera Blanca',
+            precio: 200,
+            stock: 20,
+            id: 2,
+        },
+        {
+            title: 'Pantalon Jeans',
+            precio: 800,
+            stock: 8,
+            id: 3,
+        },
+        {
+            title: 'Campera',
+            precio: 1200,
+            stock: 4,
+            id: 4,
+        },
+
+    ]
+
+const getProducts = () => {
+    return new Promise ( (resolve, reject) => {
+        setTimeout ( () => {
+            resolve (productos)
+        }, 2000)
+    } )
+}
+
+useEffect (() => {
+
+    getProducts ()
+    .then ((response) => {
+        setProducts(response)
+    })
+    .catch ((err) => {
+        console.log("Catch fallo", err)
+    })
+
+}, [])
+
+
+
     return (
        
         <Grid container spacing={2}>
-            <Grid item xs={8}>
-                <BasicCard texto1="Texto 1" texto2="Este es el card 1"/>
-            </Grid>
-            <Grid item xs={4}>
-                <BasicCard texto1="Texto 2" texto2="Este es el card 2"/>
-            </Grid>
-            <Grid item xs={4}>
-                <BasicCard texto1="Texto 3" texto2="Este es el card 3"/>
-            </Grid>
-            <Grid item xs={8}>
-                <BasicCard texto1="Texto 4" texto2="Este es el card 4"/>
-            </Grid>
+                    {console.log (products)}
+
+            {
+                products.map ((item) => {
+
+                    return (
+                        <Grid item xs={6} key={item.id}>
+                        <BasicCard texto1={item.title} texto2={item.precio}/>
+                        </Grid>
+                    )
+
+                    
+                })
+
+            }
+
         </Grid>
 
     )
